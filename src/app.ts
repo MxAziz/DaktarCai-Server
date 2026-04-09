fe68bf9c3dec9880cc73edab9e1ed3233d9e6668
MxAziz <hereabdulaziz@gmail.com>
2026-04-09 18:25:10 +0600

    first commit

---
+++ src/app.ts
@@ -0,0 +1,31 @@
+import express, { Application, NextFunction, Request, Response } from 'express';
+import cors from 'cors';
+import globalErrorHandler from './app/middlewares/globalErrorHandler';
+import notFound from './app/middlewares/notFound';
+import { uptime } from 'process';
+
+const app: Application = express();
+app.use(cors({
+    origin: 'http://localhost:3000',
+    credentials: true
+}));
+
+//parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+app.get('/', (req: Request, res: Response) => {
+    res.send({
+        Message: "DaktarCai Server is running",
+        environment: process.env.NODE_ENV || 'development',
+        uptime: process.uptime().toFixed(2) + ' seconds',
+    })
+});
+
+
+app.use(globalErrorHandler);
+
+app.use(notFound);
+
+export default app;
