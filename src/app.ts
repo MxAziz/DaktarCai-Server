aa118221294aa1aaaf07bb2b3a232fb314dc3f89
MxAziz <hereabdulaziz@gmail.com>
2026-05-20 23:14:54 +0600

    added user related module

--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,10 @@ import express, { Application, NextFunction, Request, Response } from 'express';
 import cors from 'cors';
 import globalErrorHandler from './app/middlewares/globalErrorHandler';
 import notFound from './app/middlewares/notFound';
+import router from './app/routes';
 
 const app: Application = express();
+
 app.use(cors({
     origin: 'http://localhost:3000',
     credentials: true
@@ -12,6 +14,8 @@ app.use(cors({
 //parser
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use('/api/v1', router);
 
 
 app.get('/', (req: Request, res: Response) => {
